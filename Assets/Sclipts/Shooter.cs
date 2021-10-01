311df049f02cbea23647170742dfa70036e7e854
zel01 <c20009@std.it-college.ac.jp>
2021-10-01 09:46:54 +0900

    現状保存のため一旦コミット。更新はそこまで無い

---
+++ Assets/Sclipts/Shooter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    [SerializeField] float ShotSpeed;
+    public GameObject FiringBlockPrefab;
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1")) Shot();
+    }
+
+    public void Shot()
+    {
+        GameObject Bullet = (GameObject)Instantiate(FiringBlockPrefab,transform.position,Quaternion.identity);
+        Rigidbody2D BulletRigidBody2D = Bullet.GetComponent<Rigidbody2D>();
+        BulletRigidBody2D.AddForce(Vector2.one * ShotSpeed);
+    }
+}
