142c08fdfeb48b2ad099bd26fd2a67cd2ff83999
DESKTOP-K9V8NLR\c20019 <c20019@std.it-college.ac.jp>
2021-10-05 15:57:01 +0900

    メッセボード完成

---
+++ Assets/Sclipts/AnimationManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationManager : MonoBehaviour
+{
+    [SerializeField] Animator animator;
+
+    public void BlickStart()
+    {
+        animator.SetTrigger("isopen");
+    }
+
+    public void BlinkStop()
+    {
+        animator.SetTrigger("isstop");
+    }
+}
--- Assets/Sclipts/MessageBoard.cs
+++ Assets/Sclipts/MessageBoard.cs
@@ -1,18 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
 using UnityEngine.UI;
 public class MessageBoard : MonoBehaviour
 {
     [Header("メッセージ"), SerializeField, TextArea(1, 6)]
     string message;
-    [SerializeField] BlurChanger blur;
     [SerializeField] MessageList messageList;
     [SerializeField] Text text;
 
     [SerializeField] int messageID;
 
-
+    [SerializeField] Animator messageAnimator;
+    [SerializeField] Animator blurAnimator;
     GameObject target;
     bool isopend;
     bool isEnter;
@@ -59,19 +60,30 @@ public class MessageBoard : MonoBehaviour
 
     void OpenMessage()
     {
-        if (text.text == null)
-        {
-            Debug.Log("事前にメッセージを追加");
-            text.text = messageList.messages[messageID].message;
-        }   
-        blur.OnBlur();
+
+
+        GetMessage();
+        blurAnimator.SetTrigger("start");
         isopend = true;
+        messageAnimator.SetTrigger("start");
+        
+        Time.timeScale = 0;
+        
     }
     void CloseMessage()
     {
-        blur.OnNormal();
+     
+        blurAnimator.SetTrigger("stop");
+        messageAnimator.SetTrigger("stop");
         isopend = false;
         isEnter = false;
+        Time.timeScale = 1;
 
+    }
+
+    public void GetMessage()
+    {
+        Debug.Log("事前にメッセージを追加");
+        text.text = messageList.messages[messageID].message;
     }
 }
